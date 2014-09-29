a58fa5e93e11a3dae423a4b21f1d1dd81ade3185
Cooper <971599336@qq.com>
2014-09-29 19:45:45 +0800

    1
    1

--- CSDialog/src/org/csodev/csdialog/MainActivity.java
+++ CSDialog/src/org/csodev/csdialog/MainActivity.java
@@ -51,33 +51,7 @@ public class MainActivity extends Activity {
 			}
 		});
 		
-		
-//		/**
-//		 * 1：progress dialog
-//		 */
-//		CSProgressDialog csDialog = new CSProgressDialog(this);
-//		csDialog.setCancelable(true);//物理返回键是否关闭
-//		csDialog.show();
-//		csDialog.dismiss();
-//		
-//		/**
-//		 * 2：带有一个按钮的dialog
-//		 */
-//		CSOKDialog.createBuilder(this).setMsg("这是一条信息").show();
-//		
-//		/**
-//		 * 3:两个按钮的diallog
-//		 */
-//		final CSOKCancelDialog okCancelDialog = CSOKCancelDialog.createBuilder(this).setMsg("这是一条信息");
-//		okCancelDialog.setOKOnClickListener(new OnClickListener() {
-//			
-//			@Override
-//			public void onClick(View view) {
-//				okCancelDialog.dismiss();
-//				//TODO...
-//			}
-//		});
-//		okCancelDialog.show();
+	 
 		
 	}
 
